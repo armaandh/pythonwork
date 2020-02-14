531bd98b4bafbcaea37b95e0613a72896f195d31
armaandh <dhanjiarmaan@gmail.com>
2020-02-14 10:56:18 -0800

    this is my commit message

---
+++ app.py
@@ -0,0 +1,14 @@
+class person:
+    def __init__(self):
+        self.__name=''
+        self.__creditcardnumber = ''
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    def name(self, value):
+        self.__name=value
+
+p = person()
+p.name = "Steve jobs"
+print(p.name)
